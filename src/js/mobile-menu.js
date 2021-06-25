f153293c7f100fa26d8f397dc8c42d760df56726
OLgaPonomarenko <fblackberries@gmail.com>
2021-06-26 01:42:48 +0300

    header complete

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,14 +2,16 @@
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
     const mobileBtnClose = document.querySelector("[data-menu-close]");
+
   
     menuBtnRef.addEventListener("click", () => {
+
       mobileMenuRef.classList.add("is-open");
-       mobileMenuRef.classList.add("mobile-menu__item");
+ 
     })
 
     mobileBtnClose.addEventListener('click', () => {
       mobileMenuRef.classList.remove("is-open");
-      mobileMenuRef.classList.remove("mobile-menu__item");
+    
     });
   })()
