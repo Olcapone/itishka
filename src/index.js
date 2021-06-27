b4ae16007f490bcd2e6d17ca19cf7021e4805a99
OLgaPonomarenko <fblackberries@gmail.com>
2021-06-27 15:01:45 +0300

    tampletes for example

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import './sass/main.scss';
 
 import './js/mobile-menu.js';
 
+// шаблонная строка на карточки курсов
+
+// import template from './productList.hbs';
+// const products = require('./courses.json');
+
+// const cardList = document.querySelector(".card_list-js");
+// cardList.insertAdjacentHTML('afterbegin', template(products));
+
+
 
