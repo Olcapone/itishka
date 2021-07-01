59972c59505c1d793ac70b5283b3b2cfecbce6bd
OLgaPonomarenko <fblackberries@gmail.com>
2021-07-01 04:03:10 +0300

    complete skript

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,5 +1,6 @@
 /* Style the buttons that are used to open and close the accordion panel */
 let acc = document.getElementsByClassName("question__item");
+// найти крестик
 let i;
 
 for (i = 0; i < acc.length; i++) {
@@ -7,6 +8,9 @@ for (i = 0; i < acc.length; i++) {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
+
+    this.lastElementChild.classList.toggle("active_icon");
+
 
     /* Toggle between hiding and showing the active panel */
     let panel = this.nextElementSibling;
